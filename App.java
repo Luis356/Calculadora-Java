cac81b55972f111e02b2789bcb0753ad3a59102a
Luis Ricardo <56393737+Luis356@users.noreply.github.com>
2023-07-16 17:13:50 -0300

    att App

--- App.java
+++ App.java
@@ -4,10 +4,20 @@ public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Historico historico = new Historico();
-
+        boolean primeiraEx = true;
         while (true) {
 
-            System.out.println("Digite uma expressão: ");
+            if (primeiraEx) {
+
+                System.out.println("Digite uma expressão: ");
+                primeiraEx = false;
+
+            } else {
+
+                System.out.println("Digite outra expressão ou 'H' para ver o Historico: ");
+
+            }
+
             String resp = scanner.nextLine();
 
             if (resp.equalsIgnoreCase("h")) {
