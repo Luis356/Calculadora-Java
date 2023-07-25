27cbf17d05c87e2f80698945d683441a84a1c38a
Luis Ricardo <56393737+Luis356@users.noreply.github.com>
2023-07-25 11:26:00 -0300

    add interface

--- App.java
+++ App.java
@@ -1,33 +1,63 @@
-import java.util.Scanner;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class App {
+public class App extends Application {
+    private Historico historico;
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Historico historico = new Historico();
-        boolean primeiraEx = true;
-        while (true) {
-
-            if (primeiraEx) {
-
-                System.out.println("Digite uma expressão: ");
-                primeiraEx = false;
-
-            } else {
-
-                System.out.println("Digite outra expressão ou 'H' para ver o Historico: ");
-
-            }
-
-            String resp = scanner.nextLine();
-
-            if (resp.equalsIgnoreCase("h")) {
-                historico.exibirHistorico();
-                break;
-
-            } else {
-                new Recebe(resp, historico);
-            }
-        }
-        scanner.close();
+        launch(args);
     }
-}
+
+    @Override
+    public void start(Stage primaryStage) {
+        historico = new Historico();
+
+        BorderPane root = new BorderPane();
+        root.setPadding(new Insets(10));
+
+        // Exibir o histórico
+        TextArea historicoTextArea = new TextArea();
+        historicoTextArea.setEditable(false);
+        atualizarHistoricoTextArea(historicoTextArea);
+
+        // Exibir a mensagem
+        Label label = new Label("Digite uma expressão:");
+
+        // Inserir a expressão
+        TextArea expressaoTextArea = new TextArea();
+
+        // Botão para processar a expressão
+        Button calcularButton = new Button("Calcular");
+        calcularButton.setOnAction(e -> {
+            String expressao = expressaoTextArea.getText();
+            new Recebe(expressao, historico);
+            atualizarHistoricoTextArea(historicoTextArea);
+            expressaoTextArea.clear();
+        });
+
+        // Layout para os componentes
+        VBox vbox = new VBox(10, label, expressaoTextArea, calcularButton);
+        HBox hbox = new HBox(10, vbox, historicoTextArea);
+        root.setCenter(hbox);
+
+        Scene scene = new Scene(root, 400, 300);
+        primaryStage.setTitle("Calculadora");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private void atualizarHistoricoTextArea(TextArea historicoTextArea) {
+        historicoTextArea.clear();
+        for (String item : historico.getHistorico()) {
+            historicoTextArea.appendText(item + "\n");
+        }
+    }
+} 
